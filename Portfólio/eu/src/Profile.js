b1e0b9555aaa13de42ed1f479d14acbf6d490008
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-24 10:12:15 -0300

    ajustando links e img

--- Portfólio/eu/src/Profile.js
+++ Portfólio/eu/src/Profile.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import './Profile.css'
 
 function Profile() {
   return (
     <div className='banner'>
+
+
         <div className="intro">
+          
           <div className="title">
             <h1>Olá, eu sou o </h1>
             <span>Rhuan Bello :)</span>
             <p>Desenvolvedor Front-End</p>
           </div>
           <div className="actions">
-            <a href="assets/cv/cv-rhuanbello.pdf" download>Download CV</a>
-            <a href="mailto:rhuambello@gmail.com" target="_blank" rel="noopener noreferrer">Entrar em contato</a>
+            <a href="arq/Curriculo.pdf" download>Download CV</a>
+            <a href="mailto:wallames@live.com" target="_blank" rel="noopener noreferrer">Entrar em contato</a>
           </div>
         </div>
+
         <div className="profile">
-          <a href="https://github.com/rhuanbello" target="_blank" rel="noopener noreferrer">
-            <img src="assets/avatar2.png" alt="avatar" />
+          <a href="https://github.com/wallames1" target="_blank" rel="noopener noreferrer">
+            <img src="img/eu.png" alt="avatar" />
           </a>
         </div>
     </div>
