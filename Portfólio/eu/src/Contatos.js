868b624ef802e800cf32cbea7242517c8eff3dff
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-26 11:59:10 -0300

    adicionando contato

--- Portfólio/eu/src/Contatos.js
+++ Portfólio/eu/src/Contatos.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import './Contatos.css'
 
 function Contatos() {
   return (
     <div>
-        <h1>CONTATOS</h1>
+        <h1 >Contatos</h1>
+        
+        <div className='container-contato'>
+
+
+
+        <div className='div-contatos'>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+          <img src="img/icons/email-quadrado.png" alt="email" />
+          </a>
+        </div>
+
+        <div className='div-contatos'>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+          <img src="img/icons/linkedin-quadrado.png" alt="" />
+          </a>
+        </div>
+
+        <div className='div-contatos'>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+          <img src="img/icons/tell-quadrado.png" alt="" />
+          </a>
+        </div>
+
+        <div className='div-contatos'>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+          <img src="img/icons/github-quadrado.png" alt="" />
+          </a>
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </div>
+
     </div>
   )
 }
