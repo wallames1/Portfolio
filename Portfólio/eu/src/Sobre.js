22560316d26e6e7b39f562e81c088e26af17f541
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-25 22:22:14 -0300

    add sobre

--- Portfólio/eu/src/Sobre.js
+++ Portfólio/eu/src/Sobre.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Sobre.css'
 
 function Sobre() {
   return (
-    <div>
-        <h1>SOBRE</h1>
+    <div className='sobre'>
+        <h1>Sobre mim</h1>
+        <p>"Desenvolvedor Web com experiência acadêmica em React.js, JavaScript, Sql server, C# e consumo de APIs REST. Apaixonado pelo desenvolvimento de componentes reutilizáveis. Formado em Análise e Desenvolvimento de Sistemas e cursando Especialização em Front-End na Udemy."</p>
     </div>
   )
 }
