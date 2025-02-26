62f92aed1428f3af9cd1ba716264a4a35bda347b
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-26 01:37:31 -0300

    inseridas as mimagens dos proj e os links

--- Portfólio/eu/src/Projetos.js
+++ Portfólio/eu/src/Projetos.js
@@ -3,60 +3,39 @@ import './Projetos.css'
 
 function Projetos() {
   return (
-    <div className='div-pai'>
-      <h1>Habilidades</h1>
-    <div className='container'>
+    <div className='div-proj'>
+      <h1>Projetos</h1>
+    <div className='container-proj'>
         
 
-        <div className='box'>
+        <div className='ban'>
           
-          <div className='logo'>
-            <p>JavaScript</p>
-            <img src="img/icons/js.png" alt="logo"/>
+          <div className='proj'>
+            <h3>Fazenda Urbana</h3>
+            <a href="https://github.com/wallames1/fazenda-urbana" target="_blank" rel="noopener noreferrer">
+            <img src="img/fazenda.png" alt="logo"/>
+            </a>
           </div>
         </div>
 
-        <div className='box'>
+        <div className='ban'>
           
-          <div className='logo'>
-            <p>React</p>
-            <img src="img/icons/react.png" alt="logo"/>
+          <div className='proj'>
+            <h3>Caça Palavras</h3>
+            <a href="https://github.com/wallames1/Aulas-React/tree/main/6_SECRET_WORD/secretword" target="_blank" rel="noopener noreferrer">
+            <img src="img/secret.png" alt="logo"/>
+            </a>
           </div>
         </div>
 
 
-        <div className='box'>
+        <div className='ban'>
           
-          <div className='logo'>
-            <p>Html</p>
-            <img src="img/icons/html.png" alt="logo"/>
-          </div>
-
-        </div>
-
-        <div className='box'>
-          
-          <div className='logo'>
-            <p>Css</p>
-            <img src="img/icons/css.png" alt="logo"/>
-          </div>
-
-        </div>
-
-        <div className='box'>
-          
-          <div className='logo'>
-            <p>Sql Server</p>
-            <img src="img/icons/sql.png" alt="logo"/>
-          </div>
-
-        </div>
-
-        <div className='box'>
-          
-          <div className='logo'>
-            <p>C#</p>
-            <img src="img/icons/c.png" alt="logo"/>
+          <div className='proj'>
+            <h3>Museu Multitematico</h3>
+            <a href="https://github.com/wallames1/PIM-museu-multe-tem-tico" target="_blank" rel="noopener noreferrer">
+            <img src="img/museu.png" alt="logo"/>
+            </a>
           </div>
 
         </div>
