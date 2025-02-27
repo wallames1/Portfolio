116b52de32d2303ae908f078a779eba499a3aaad
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-27 13:29:57 -0300

    ddd

---
+++ Portfólio/eu/Portfólio/eu/src/Contatos.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import './Contatos.css'
+
+
+function Contatos() {
+  return (
+    <div id='Contato'>
+        <h1 className='titulos'>Contatos</h1>
+        
+        <div className='container-contato'>
+
+
+
+        <div id='email' className='div-contatos'>
+          <a href="mailto:wallames@live.com" target="_blank" rel="noopener noreferrer">
+          <img src={"/img/icons/email-quadrado.png"} alt="email" 
+          onMouseEnter={(e) => e.currentTarget.src = "/img/icons/email-redondo.png"}
+          onMouseLeave={(e) => e.currentTarget.src = "/img/icons/email-quadrado.png"}
+          />
+          </a>
+        </div>
+
+        <div id='linkedin' className='div-contatos'>
+          <a href="https://www.linkedin.com/in/wallames/" target="_blank" rel="noopener noreferrer">
+          <img src="/img/icons/linkedin-quadrado.png" alt="" 
+           onMouseEnter={(e) => e.currentTarget.src = "/img/icons/linkedin-redondo.png"}
+           onMouseLeave={(e) => e.currentTarget.src = "/img/icons/linkedin-quadrado.png"}
+           />
+          </a>
+        </div>
+
+        <div id='tell' className='div-contatos'>
+          <a href="https://api.whatsapp.com/send?phone=5513996545323" target="_blank" rel="noopener noreferrer">
+          <img src="/img/icons/tell-quadrado.png" alt="" 
+           onMouseEnter={(e) => e.currentTarget.src = "/img/icons/tell-redondo.png"}
+           onMouseLeave={(e) => e.currentTarget.src = "/img/icons/tell-quadrado.png"}
+           />
+          </a>
+        </div>
+
+        <div id='github' className='div-contatos'>
+          <a href="https://github.com/wallames1" target="_blank" rel="noopener noreferrer">
+          <img src="/img/icons/github-quadrado.png" alt="" 
+           onMouseEnter={(e) => e.currentTarget.src = "/img/icons/github-redondo.png"}
+           onMouseLeave={(e) => e.currentTarget.src = "/img/icons/github-quadrado.png"}
+           />
+          </a>
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </div>
+
+    </div>
+  )
+}
+
+export default Contatos
---
+++ Portfólio/eu/Portfólio/eu/src/Projetos.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Projetos.css'
+
+function Projetos() {
+  return (
+    <div id='Projetos' className='div-proj'>
+      <h1 className='titulos'>Projetos</h1>
+    <div className='container-proj'>
+        
+
+        <div className='ban'>
+          
+          <div className='proj'>
+            <h3>Fazenda Urbana</h3>
+            <a href="https://github.com/wallames1/fazenda-urbana" target="_blank" rel="noopener noreferrer">
+            <img src="img/fazenda.png" alt="logo"/>
+            </a>
+          </div>
+        </div>
+
+        <div className='ban'>
+          
+          <div className='proj'>
+            <h3>Caça Palavras</h3>
+            <a href="https://github.com/wallames1/Aulas-React/tree/main/6_SECRET_WORD/secretword" target="_blank" rel="noopener noreferrer">
+            <img src="img/secret.png" alt="logo"/>
+            </a>
+          </div>
+        </div>
+
+
+        <div className='ban'>
+          
+          <div className='proj'>
+            <h3>Museu Multitematico</h3>
+            <a href="https://github.com/wallames1/PIM-museu-multe-tem-tico" target="_blank" rel="noopener noreferrer">
+            <img src="img/museu.png" alt="logo"/>
+            </a>
+          </div>
+
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Projetos
