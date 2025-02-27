bddc622cf72eea3215c20a76ec813b94f7cde639
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-27 15:19:16 -0300

    removi o caminho icons

--- Portfólio/eu/src/Contatos.js
+++ Portfólio/eu/src/Contatos.js
@@ -13,36 +13,36 @@ function Contatos() {
 
         <div id='email' className='div-contatos'>
           <a href="mailto:wallames@live.com" target="_blank" rel="noopener noreferrer">
-          <img src={"/img/icons/email-quadrado.png"} alt="email" 
-          onMouseEnter={(e) => e.currentTarget.src = "/img/icons/email-redondo.png"}
-          onMouseLeave={(e) => e.currentTarget.src = "/img/icons/email-quadrado.png"}
+          <img src={"/img/email-quadrado.png"} alt="email" 
+          onMouseEnter={(e) => e.currentTarget.src = "/img/email-redondo.png"}
+          onMouseLeave={(e) => e.currentTarget.src = "/img/email-quadrado.png"}
           />
           </a>
         </div>
 
         <div id='linkedin' className='div-contatos'>
           <a href="https://www.linkedin.com/in/wallames/" target="_blank" rel="noopener noreferrer">
-          <img src="/img/icons/linkedin-quadrado.png" alt="linkedin" 
-           onMouseEnter={(e) => e.currentTarget.src = "/img/icons/linkedin-redondo.png"}
-           onMouseLeave={(e) => e.currentTarget.src = "/img/icons/linkedin-quadrado.png"}
+          <img src="/img/linkedin-quadrado.png" alt="linkedin" 
+           onMouseEnter={(e) => e.currentTarget.src = "/img/linkedin-redondo.png"}
+           onMouseLeave={(e) => e.currentTarget.src = "/img/linkedin-quadrado.png"}
            />
           </a>
         </div>
 
         <div id='tell' className='div-contatos'>
           <a href="https://api.whatsapp.com/send?phone=5513996545323" target="_blank" rel="noopener noreferrer">
-          <img src="/img/icons/tell-quadrado.png" alt="tell" 
-           onMouseEnter={(e) => e.currentTarget.src = "/img/icons/tell-redondo.png"}
-           onMouseLeave={(e) => e.currentTarget.src = "/img/icons/tell-quadrado.png"}
+          <img src="/img/tell-quadrado.png" alt="tell" 
+           onMouseEnter={(e) => e.currentTarget.src = "/img/tell-redondo.png"}
+           onMouseLeave={(e) => e.currentTarget.src = "/img/tell-quadrado.png"}
            />
           </a>
         </div>
 
         <div id='github' className='div-contatos'>
           <a href="https://github.com/wallames1" target="_blank" rel="noopener noreferrer">
-          <img src="/img/icons/github-quadrado.png" alt="github" 
-           onMouseEnter={(e) => e.currentTarget.src = "/img/icons/github-redondo.png"}
-           onMouseLeave={(e) => e.currentTarget.src = "/img/icons/github-quadrado.png"}
+          <img src="/img/github-quadrado.png" alt="github" 
+           onMouseEnter={(e) => e.currentTarget.src = "/img/github-redondo.png"}
+           onMouseLeave={(e) => e.currentTarget.src = "/img/github-quadrado.png"}
            />
           </a>
         </div>
