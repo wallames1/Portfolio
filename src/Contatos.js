7d254282a6bbb50ae3aaa82d5daa23d613c7dabc
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-28 23:56:22 -0300

    Atualizar gh-pages com a versão mais recente

--- src/Contatos.js
+++ src/Contatos.js
@@ -14,8 +14,7 @@ function Contatos() {
         <div id='email' className='div-contatos'>
           <a href="mailto:wallames@live.com" target="_blank" rel="noopener noreferrer">
           <img src={"/img/email-quadrado.png"} alt="email" 
-          onMouseEnter={(e) => e.currentTarget.src = "/img/email-redondo.png" }
-          onMouseLeave={(e) => e.currentTarget.src = "/img/email-quadrado.png"}
+          
           />
           </a>
         </div>
@@ -23,8 +22,7 @@ function Contatos() {
         <div id='linkedin' className='div-contatos'>
           <a href="https://www.linkedin.com/in/wallames/" target="_blank" rel="noopener noreferrer">
           <img src="/img/linkedin-quadrado.png" alt="linkedin" 
-           onMouseEnter={(e) => e.currentTarget.src = "/img/linkedin-redondo.png"}
-           onMouseLeave={(e) => e.currentTarget.src = "/img/linkedin-quadrado.png"}
+          
            />
           </a>
         </div>
@@ -32,17 +30,15 @@ function Contatos() {
         <div id='tell' className='div-contatos'>
           <a href="https://api.whatsapp.com/send?phone=5513996545323" target="_blank" rel="noopener noreferrer">
           <img src="/img/tell-quadrado.png" alt="tell" 
-           onMouseEnter={(e) => e.currentTarget.src = "/img/tell-redondo.png"}
-           onMouseLeave={(e) => e.currentTarget.src = "/img/tell-quadrado.png"}
+           
            />
           </a>
         </div>
 
         <div id='github' className='div-contatos'>
           <a href="https://github.com/wallames1" target="_blank" rel="noopener noreferrer">
-          <img src="/img/github-quadrado.png" alt="github" 
-           onMouseEnter={(e) => e.currentTarget.src = "/img/github-redondo.png"}
-           onMouseLeave={(e) => e.currentTarget.src = "/img/github-quadrado.png"}
+          <img src="img/github-quadrado.png" alt="github" 
+           
            />
           </a>
         </div>
