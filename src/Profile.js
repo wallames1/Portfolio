94a8b839f59a7333ebcd14566e5584aef66f9ea1
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-28 22:47:07 -0300

    Primeiro commit na gh-pages

---
+++ src/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Profile.css'
+
+function Profile() {
+  return (
+    <div className='banner'>
+
+
+        <div className="intro">
+          
+          <div className="title">
+            <h1>Olá, eu sou o </h1>
+            <span>Wallames Anjos :)</span>
+            <p>Desenvolvedor Web</p>
+          </div>
+          <div className="actions">
+            <a className='BtnDownload' href="arq/Curriculo.pdf" download>Download CV</a>
+            <a className='BtnContato' href="mailto:wallames@live.com" target="_blank" rel="noopener noreferrer">Entrar em contato</a>
+          </div>
+        </div>
+
+        <div className="profile">
+          <a href="https://github.com/wallames1" target="_blank" rel="noopener noreferrer">
+            <img src="img/eu.png" alt="avatar" />
+          </a>
+        </div>
+    </div>
+  );
+}
+
+export default Profile;
