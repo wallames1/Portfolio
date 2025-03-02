f6bd1508fc4fae1039e674aeed81d5f207fb4c19
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-03-02 12:02:00 -0300

    Update Sobre.js

--- Portfólio/eu/src/Sobre.js
+++ Portfólio/eu/src/Sobre.js
@@ -5,7 +5,7 @@ function Sobre() {
   return (
     <div id='Sobre'  className='sobre'>
         <h1 className='titulos'>Sobre mim</h1>
-        <p>"Desenvolvedor Web com experiência acadêmica em React.js, JavaScript, Sql server, C# e consumo de APIs REST. Apaixonado pelo desenvolvimento de componentes reutilizáveis. Formado em Análise e Desenvolvimento de Sistemas e cursando Especialização em Front-End na Udemy."</p>
+        <p>"Desenvolvedor Web com experiência acadêmica em React.js, JavaScript, Sql server, C# e consumo de APIs REST. Apaixonado pelo desenvolvimento de componentes reutilizáveis. Formado em Análise e Desenvolvimento de Sistemas e cursando Especialização em Desenvolvimento Web na Udemy."</p>
     </div>
   )
 }
