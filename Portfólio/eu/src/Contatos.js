ce955f07348f63399fe93298e55766fd045aba17
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-03-01 22:35:00 -0300

    removido o jv de contatos

--- Portfólio/eu/src/Contatos.js
+++ Portfólio/eu/src/Contatos.js
@@ -13,36 +13,25 @@ function Contatos() {
 
         <div id='email' className='div-contatos'>
           <a href="mailto:wallames@live.com" target="_blank" rel="noopener noreferrer">
-          <img src={"/img/email-quadrado.png"} alt="email" 
-          onMouseEnter={(e) => e.currentTarget.src = "/img/email-redondo.png" }
-          onMouseLeave={(e) => e.currentTarget.src = "/img/email-quadrado.png"}/>
+          <img src="/img/email-quadrado.png" alt="email"/>
           </a>
         </div>
 
         <div id='linkedin' className='div-contatos'>
           <a href="https://www.linkedin.com/in/wallames/" target="_blank" rel="noopener noreferrer">
-          <img src="/img/linkedin-quadrado.png" alt="linkedin" 
-           onMouseEnter={(e) => e.currentTarget.src = "/img/linkedin-redondo.png"}
-           onMouseLeave={(e) => e.currentTarget.src = "/img/linkedin-quadrado.png"}
-           />
+          <img src="/img/linkedin-quadrado.png" alt="linkedin"/>
           </a>
         </div>
 
         <div id='tell' className='div-contatos'>
           <a href="https://api.whatsapp.com/send?phone=5513996545323" target="_blank" rel="noopener noreferrer">
-          <img src="/img/tell-quadrado.png" alt="tell" 
-           onMouseEnter={(e) => e.currentTarget.src = "/img/tell-redondo.png"}
-           onMouseLeave={(e) => e.currentTarget.src = "/img/tell-quadrado.png"}
-           />
+          <img src="/img/tell-quadrado.png" alt="tell" />
           </a>
         </div>
 
         <div id='github' className='div-contatos'>
           <a href="https://github.com/wallames1" target="_blank" rel="noopener noreferrer">
-          <img src="/img/github-quadrado.png" alt="github" 
-           onMouseEnter={(e) => e.currentTarget.src = "/img/github-redondo.png"}
-           onMouseLeave={(e) => e.currentTarget.src = "/img/github-quadrado.png"}
-           />
+          <img src="/img/github-quadrado.png" alt="github"/>
           </a>
         </div>
         
